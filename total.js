d3297529ae40aa46f8a019b4749305262c44ea4c
ishtiak777 <ishtiak777@gmail.com>
2016-02-11 05:59:22 +0600

    Update total.js

--- total.js
+++ total.js
@@ -65,9 +65,9 @@ for(var j=randTitle.length-1;j>=0;j--){
 // outputting the result
 
 if(mVersion==1){
-//	document.write('<h3>Most viewd</h3>');
-//	document.write('<ul>'+finalString+'</ul>');
+/	document.write('<h3>Most viewd</h3>');
+	document.write('<ul>'+finalString+'</ul>');
 
-	document.write('<h3>Latest posts</h3>');
-	document.write('<ul>'+randFinal+'</ul>');
+//	document.write('<h3>Latest posts</h3>');
+//	document.write('<ul>'+randFinal+'</ul>');
 }
